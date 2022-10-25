b90033c782f6af5c1093897d63e234b210198050
williambjork <william.bjork@gobil.se>
2022-10-25 14:15:54 +0200

    added navbar

--- components/Nav.js
+++ components/Nav.js
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 export default function Nav() {
   return (
-    <nav>
-        <ul>
+    <nav className="text-2xl font-medium">
+        <ul className="flex gap-12">
             <Link href="/"><li>Home</li></Link>
             <Link href="/contact"><li>Contact</li></Link>
         </ul>
