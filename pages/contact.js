458a88e5fbf336e7576c9d3dd8c1ead38f282498
williambjork <william.bjork@gobil.se>
2022-10-25 14:26:06 +0200

    added contakt page, styling

---
+++ pages/contact.js
@@ -0,0 +1,19 @@
+import { motion as m } from "framer-motion"
+
+function contact() {
+  return (
+    <main>
+        <div>
+            <h1>let's talk</h1>
+        </div>
+
+        <div>
+            <div>
+                
+            </div>
+        </div>
+    </main>
+  )
+}
+
+export default contact
